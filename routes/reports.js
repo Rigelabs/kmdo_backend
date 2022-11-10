a71a476028d7bb1036ffb29fe2390b3fa8f2ea40
rigelsolutions@outlook.com <rigelsolutions@outlook.com>
2022-11-11 01:44:29 +0300

    reports updated

--- routes/reports.js
+++ routes/reports.js
@@ -8,32 +8,36 @@ const router = express.Router();
 const ReportsCollection=ReportsConnection.collection("Reports");
 
 
-router.post('/create',ensureActive,ensureAdmin, async (req, res) => {
+router.post('/create', ensureAuth, ensureActive, ensureAdmin,async (req, res) => {
 
    
     try {
        
         const { title,link } = req.body;
-      
+        if(title && link){
         await ReportsCollection.findOne({title: title}).then(report => {
-            if (area) {
+            if (report) {
                 return res.status(400).json({ message: `The report already exist` })
             }
             ReportsCollection.insertOne({
                     title: title,
-                    link:link
+                    link:link,
+                    createdAt:new Date
                 }).then(saved=>{
 
                 return res.status(200).json({ message: "Report registered successfully" });
             });
         });
+        }else{
+            res.status(400).json({message:"Invalid request"});
+        }
     } catch (error) {
         logger.error(`Error creating report, ${error}`);
         return res.status(400).json({ message: error.message })
     }
 });
 router.get("/all", async (req, res) => {
-    await ReportsCollection.find().toArray().then(reports=>{
+    await ReportsCollection.find({},{sort:{"createdAt":-1}}).toArray().then(reports=>{
         res.status(200).json(reports);
     })
 });
